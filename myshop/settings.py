d5b0dcb41a64b5f850fa08dfde32c82b9f1787f4
Mihail Pertsev <vsalyanava@bk.ru>
2019-06-28 05:56:24 +0300

    still yrting

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -168,5 +172,3 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
